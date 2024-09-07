f1e72f2757a84c3a8484464898cbcda39b7f0455
Ahmed Elzagh <ahmedelzagh50@gmail.com>
2024-09-07 17:29:26 +0300

    chore: Update backend.tf and variables.tf for Terraform state management

--- backend.tf
+++ backend.tf
@@ -1,34 +1,34 @@
-resource "aws_s3_bucket" "statebucket" {
-  bucket = "ahmedelzagh-state-bucket"
+# resource "aws_s3_bucket" "statebucket" {
+#   bucket = "ahmedelzagh-state-bucket"
 
-  tags = {
-    Name = "state bucket"
-  }
-}
+#   tags = {
+#     Name = "state bucket"
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "terraform_state_bucket" {
-  bucket = aws_s3_bucket.statebucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "terraform_state_bucket" {
+#   bucket = aws_s3_bucket.statebucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "terraform-state-locks"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "LockID"
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name           = "terraform-state-locks"
+#   billing_mode   = "PROVISIONED"
+#   read_capacity  = 1
+#   write_capacity = 1
+#   hash_key       = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
 
-  tags = {
-    Name = "Terraform State Lock Table"
-  }
-}
+#   tags = {
+#     Name = "Terraform State Lock Table"
+#   }
+# }
 
 terraform {
   backend "s3" {
